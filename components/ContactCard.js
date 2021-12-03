4b57f1af935a64bdccaa1912925914f102513cc5
Skyler Knight <31410434+skylerknight@users.noreply.github.com>
2021-12-03 00:50:15 -0600

    Updated contact card styles

--- components/ContactCard.js
+++ components/ContactCard.js
@@ -1,12 +1,13 @@
 import React from "react";
+import { DotsVerticalIcon } from "@heroicons/react/solid";
 
 const ContactCard = ({ id, email, firstName, lastName, phoneNumber }) => {
 	return (
 		<li className="flex flex-row items-center space-x-5 p-3 shadow-md bg-white">
 			{/* Avatar */}
-			<div className="w-12 h-12 rounded-full bg-gray-100"></div>
+			<div className="flex flex-col w-12 h-12 rounded-full bg-yellow-100 "></div>
 			{/* Full Name */}
-			<div>
+			<div className="flex-1">
 				<h3 className="font-bold text-sm">
 					{firstName} {lastName}
 				</h3>
@@ -14,6 +15,10 @@ const ContactCard = ({ id, email, firstName, lastName, phoneNumber }) => {
 					<li className="text-sm text-gray-400">{phoneNumber}</li>
 				</ul>
 			</div>
+			{/* Contact Card Controls */}
+			<button className="flex w-8 h-8 rounded-full ring-offset-2 ring-blue-800 hover:bg-blue-100 hover:text-blue-800 focus:bg-blue-100 focus:text-blue-800 focus:ring-2">
+				<DotsVerticalIcon className="w-5 h-5 m-auto" />
+			</button>
 		</li>
 	);
 };
