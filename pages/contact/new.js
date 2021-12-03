9914d55de742a49db96b85a430af5970ad23c85d
Skyler Knight <31410434+skylerknight@users.noreply.github.com>
2021-12-03 16:14:03 -0600

    bug-fixed-missing-imports

--- pages/contact/new.js
+++ pages/contact/new.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Link from "next/link";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 import Layout from "../../components/Layout";
 import ContactForm from "../../components/ContactForm";
 
 const CreateContactPage = () => {
 	return (
 		<Layout>
-			<h1>Create Contact</h1>
-			<Header title="Visa Contacts" />
+			<Header title="Create Contact" />
 			<main className="flex flex-col flex-1 bg-gray-50">
 				<Link href={`/`}>
 					<a className="">Back</a>
