ef7a184135e1396bef9008fd6da69f9b8d89095f
Skyler Knight <31410434+skylerknight@users.noreply.github.com>
2021-12-04 16:11:27 -0600

    Fixed bug with centerTitle attribute on header

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,6 @@
 import React from "react";
-import { ChevronLeftIcon } from "@heroicons/react/solid";
 
 const Header = ({ leftControl, rightControl, title, centerTitle }) => {
-	const centerTitle = centerTitle || false;
-
 	return (
 		<header className="flex flex-col relative p-5">
 			<div className="flex flex-row items-center justify-between">
@@ -11,10 +8,7 @@ const Header = ({ leftControl, rightControl, title, centerTitle }) => {
 				<div className="ml-auto">{rightControl}</div>
 			</div>
 			<div className="px-5 mt-10 w-full max-w-screen-md mx-auto">
-				<h1
-					className={`text-xl text-${
-						centerTitle ? "center" : "left"
-					} font-bold px-2 text-[#242424]`}>
+				<h1 className={`text-xl ${centerTitle && "text-left"} font-bold px-2 text-[#242424]`}>
 					{title}
 				</h1>
 			</div>
