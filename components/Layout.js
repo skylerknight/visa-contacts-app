087ea4d16bf14b29f9f765aa2c78f1b28250738a
Skyler Knight <31410434+skylerknight@users.noreply.github.com>
2021-12-06 14:26:55 -0600

    Fixed screen height issue on mobile

--- components/Layout.js
+++ components/Layout.js
@@ -7,7 +7,7 @@ const Layout = ({ children }) => {
 	const { theme } = useTheme();
 	return (
 		<div
-			className={`theme-${theme} bg-base flex flex-col lg:h-screen lg:grid lg:grid-cols-3 lg:gap-5`}>
+			className={`theme-${theme} bg-base flex flex-col min-h-screen lg:grid lg:grid-cols-3 lg:gap-5`}>
 			<aside className="flex flex-col bg-primary w-full px-5 py-6 lg:flex-1 lg:m-5 lg:-mr-5 lg:rounded-lg lg:w-auto lg:col-span-1">
 				<div className="flex flex-col flex-1 items-center justify-center">
 					<VisaLogo />
