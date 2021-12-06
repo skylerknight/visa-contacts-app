229bf4c8962c7ae0404cb519f8825201da242b94
Skyler Knight <31410434+skylerknight@users.noreply.github.com>
2021-12-06 11:35:24 -0600

    Updated tag for aside element

--- components/Layout.js
+++ components/Layout.js
@@ -8,14 +8,14 @@ const Layout = ({ children }) => {
 	return (
 		<div
 			className={`theme-${theme} bg-base flex flex-col h-screen lg:grid lg:grid-cols-3 lg:gap-5`}>
-			<div className="flex flex-col bg-primary w-full px-5 py-6 lg:flex-1 lg:m-5 lg:-mr-5 lg:rounded-lg lg:w-auto lg:col-span-1">
+			<aside className="flex flex-col bg-primary w-full px-5 py-6 lg:flex-1 lg:m-5 lg:-mr-5 lg:rounded-lg lg:w-auto lg:col-span-1">
 				<div className="flex flex-col flex-1 items-center justify-center">
 					<VisaLogo />
 					<h1 className="mt-5 text-3xl text-primary-foreground" aria-hidden="true">
 						Visa Contacts
 					</h1>
 				</div>
-			</div>
+			</aside>
 			<div className="flex flex-col items-center justify-center col-span-3 pt-8 p-5 lg:col-span-2 lg:overlfow-x-hidden lg:overflow-y-scroll">
 				<div className="flex flex-col w-full h-full">{children}</div>
 			</div>
